ee777410448af7ed07736a6ba27cae451f64d9a8
暹罗 <siam1026@163.com>
2023-12-06 14:50:08 +0800

    初始化项目

--- wxapplet-siam-user/utils/gaode-libs/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  key: '484eda1376edf1af886e6622b993aaa3'
-}
-
-module.exports.Config = config;
--- wxapplet-siam-user/utils/global-config.js
+++
@@ -1,7 +0,0 @@
-export default class GlabalConfig {
-  //static baseUrl = 'https://api.siam.shop';
-  // static baseUrl = 'https://test-api.siam.shop';
-  static baseUrl = 'http://121.41.42.84:9601';
-  // static baseUrl = 'http://localhost:9020';
-  static ossUrl = 'https://siam-hangzhou.oss-cn-hangzhou.aliyuncs.com/';
-}
